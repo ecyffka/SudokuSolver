e1eb6c33666c6faf2faf0932bbfa784d1f451d76
Mizuki Shindoh <shindohm0510@gmail.com>
2023-04-09 22:21:39 -0700

    process 1 complete

--- betterSolver.pl
+++ betterSolver.pl
@@ -4,7 +4,7 @@
 
 % input: nine lists with nine numbers each, represents the nine rows of a sudoku grid from top to bottom
 % output: true if input is a completed sudoku puzzle (or provides solution to an incomplete puzzle)
-complete([A1,A2,A3,B1,B2,B3,C1,C2,C3],
+complete([[A1,A2,A3,B1,B2,B3,C1,C2,C3],
     [A4,A5,A6,B4,B5,B6,C4,C5,C6],
     [A7,A8,A9,B7,B8,B9,C7,C8,C9],
     [D1,D2,D3,E1,E2,E3,F1,F2,F3],
@@ -12,7 +12,7 @@ complete([A1,A2,A3,B1,B2,B3,C1,C2,C3],
     [D7,D8,D9,E7,E8,E9,F7,F8,F9],
     [G1,G2,G3,H1,H2,H3,I1,I2,I3],
     [G4,G5,G6,H4,H5,H6,I4,I5,I6],
-    [G7,G8,G9,H7,H8,H9,I7,I8,I9]) :- 
+    [G7,G8,G9,H7,H8,H9,I7,I8,I9]]) :- 
     check([A1,A2,A3,B1,B2,B3,C1,C2,C3]),
     check([A4,A5,A6,B4,B5,B6,C4,C5,C6]),
     check([A7,A8,A9,B7,B8,B9,C7,C8,C9]),
--- main.pl
+++ main.pl
@@ -3,6 +3,7 @@
 
 :- [betterSolver].
 :- [generator].
+:- [testPuzzles].
 
 % UI of the sudoku solver & generator application %
 
@@ -18,9 +19,10 @@
 main_menu :-
       nl,
       write('1. Solve sudoku with the application'), nl,
-      write('2. Generate a sudoku'), nl,
-      write('3. Find the difficulty of a sudoku'), nl,
-      write('Choose from the following options or type "stop.": '),
+      write('2. Get a hint for a sudpku puzzle'),
+      write('3. Generate a sudoku'), nl,
+      write('4. Find the difficulty of a sudoku'), nl,
+      write('Choose from the options above or type "stop.": '),
       read(Input),
       process(Input).
 
@@ -29,28 +31,77 @@ process(stop) :-
       !.
 
 process(1) :-
+      write('option 1'), nl,
       write('Provide a sudoku puzzle here: '),
+      % read(Sudoku),
+      % complete(Sudoku),
+      % read_sudoku(List1, List2, List3, List4, list5, List6, List7, List8, List9),
+      % complete(List1, List2, List3, List4, list5, List6, List7, List8, List9),
       read(Sudoku),
-      complete(Sudoku),
-      write('***Display the solved sudoku***'),
+      solve_puzzle(Sudoku),
       main_menu.
 
+solve_puzzle(Sudoku) :-
+      get_puzzle(Sudoku, P),
+      write('Original Puzzle'), nl,
+      displayBoard(P),
+      write('Solved Puzzle'), nl,
+      complete(P).
+
+get_puzzle(none, P) :-
+      none(P).
+
+get_puzzle(beginner, P) :-
+      beginner(P).
+/*
+sub_menu(Sudoku) :-
+      write('1. Solve the sudoku'), nl,
+      write('2. Get a hint'), nl,
+      write('Choose from the options: '),
+      read(Input),
+      process_solve_hint(Input, Sudoku).
+*/
+
 process(2) :-
+      write('option 2'), nl,
+      write('Provide a sudoku puzzle here: '),
+      read(Sudoku).
+
+
+process(3) :-
       write('Choose a difficulty - easy, medium, difficult, evil: '),
       read(Difficulty),
       generate(Puzzle, Difficulty),
       displayBoard(Puzzle),
       main_menu.
 
-process(3) :-
+process(4) :-
       write('Provide a sudoku puzzle here: '),
-      read(Sudoku),
-      difficulty(Sudoku, Difficulty),
-      write('The difficulty of the sudoku puzzle is '), write(Difficulty),
+      % read(Sudoku),
+      % difficulty(Sudoku, Difficulty),
+      % read_sudoku(List1, List2, List3, List4, list5, List6, List7, List8, List9),
+      % difficulty([List1, List2, List3, List4, list5, List6, List7, List8, List9], Difficulty),
+      % write('The difficulty of the sudoku puzzle is '), write(Difficulty),
+      write('option 3 chosen'),
       main_menu.
 
 process(Input) :-
       write('Chosen option is '), write(Input), nl,
-      write('Try again with a valid option: '),
-      read(Input),
-      process(Input).
+      write('Try again with a valid option.'),
+      main_menu.
+
+% Read a sudoku puzzle which is a list of 9 lists of numbers
+read_sudoku(A, B, C, D, E, F, G, H, I) :-
+      read(A),
+      read(B),
+      read(C),
+      read(D),
+      read(E),
+      read(F),
+      read(G),
+      read(H),
+      read(I).
+
+read_mul(A, B) :-
+      read(A),
+      read(B).
